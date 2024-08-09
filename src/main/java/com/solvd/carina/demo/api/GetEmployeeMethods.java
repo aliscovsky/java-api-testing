2cfa8fac42345acdfde46b7b349811af6c472791
aliscovsky <aliscovsky@solvd.com>
2024-08-09 12:02:35 -0300

    Implement GET method tests for API endpoints

---
+++ src/main/java/com/solvd/carina/demo/api/GetEmployeeMethods.java
@@ -0,0 +1,21 @@
+package com.solvd.carina.demo.api;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.ResponseTemplatePath;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.config.Configuration;
+
+@Endpoint(url = "https://dummy.restapiexample.com/api/v1/employee/${employee_id}", methodType = HttpMethodType.GET)
+@ResponseTemplatePath(path = "api/employees/employee/_get/rs.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class GetEmployeeMethods extends AbstractApiMethodV2 {
+	
+
+    public GetEmployeeMethods(String employeeId) {
+    	replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
+        replaceUrlPlaceholder("employee_id", employeeId);
+    }
+}
---
+++ src/test/java/com/solvd/carina/demo/APITests.java
@@ -0,0 +1,48 @@
+package com.solvd.carina.demo;
+
+import java.lang.invoke.MethodHandles;
+import java.time.temporal.ChronoUnit;
+
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import com.zebrunner.carina.core.IAbstractTest;
+import com.solvd.carina.demo.api.GetEmployeeMethods;
+import com.solvd.carina.demo.api.GetEmployeesMethods;
+import com.zebrunner.carina.api.APIMethodPoller;
+import com.zebrunner.carina.api.apitools.validation.JsonComparatorContext;
+import com.zebrunner.carina.api.apitools.validation.JsonCompareKeywords;
+import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
+import com.zebrunner.carina.core.registrar.tag.Priority;
+import com.zebrunner.carina.core.registrar.tag.TestPriority;
+
+public class APITests implements IAbstractTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test()
+    @MethodOwner(owner = "aliscovsky")
+    public void testGetEmployees() {
+        GetEmployeesMethods getEmployeesMethods = new GetEmployeesMethods();
+        getEmployeesMethods.callAPIExpectSuccess();
+        getEmployeesMethods.validateResponse(JSONCompareMode.STRICT, JsonCompareKeywords.ARRAY_CONTAINS.getKey() + "data");
+        getEmployeesMethods.validateResponseAgainstSchema("api/employees/_get/rs.schema");
+    }
+    
+    @Test()
+    @MethodOwner(owner = "aliscovsky")
+    public void testGetEmployee() {
+    	String employeeId = "1";
+    	
+        GetEmployeeMethods getEmployeeMethod = new GetEmployeeMethods(employeeId);
+        getEmployeeMethod.callAPIExpectSuccess();
+        
+        JsonComparatorContext comparatorContext = JsonComparatorContext.context()
+                .<Integer>withPredicate("idPredicate", id -> id == Integer.valueOf(employeeId).intValue());
+        getEmployeeMethod.validateResponse(comparatorContext);
+        getEmployeeMethod.validateResponseAgainstSchema("api/employees/employee/_get/rs.schema");
+    }
+
+}
